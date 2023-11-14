57409dd9157622c9ed66706d9a4f167634a4b60f
Futurespast <104465572+Futurespast@users.noreply.github.com>
2023-11-14 13:05:31 -0500

    added some more function to the authentication

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, flash, session
 
 app = Flask(__name__)
-
+app.secret_key = 'your_very_secret_key'
 
 @app.route('/')
 def index():
@@ -13,10 +13,12 @@ def login():
     # Here you will handle the login logic
     username = request.form['username']
     password = request.form['password']
-
-    # For now, let's redirect to a dummy video list page
-    return redirect(url_for('video_list'))
-
+    if username == "test" and password == "pass":
+        # For now, let's redirect to a dummy video list page
+        return redirect(url_for('video_list'))
+    else:
+        flash('Invalid username or password. Please try again.')
+        return redirect(url_for('index'))
 
 @app.route('/video_list')
 def video_list():
