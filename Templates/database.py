63058db3f1433f5bc984b6b28170587fde8759f4
Abdur Siddiqui <abdursiddiqui50@gmail.com>
2023-11-23 13:31:09 -0500

    made some chnages

---
+++ Templates/database.py
@@ -0,0 +1,55 @@
+import sqlite3
+from sqlite3 import Error
+
+
+def create_table(conn, create_table_sql):
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+    except Error as e:
+        print(e)
+
+
+if __name__ == '__main__':
+    db_file = "securityDatabase.db"
+    conn = None
+
+
+    sql_create_users_table = """
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY,
+        username TEXT NOT NULL,
+        password TEXT NOT NULL
+    );"""
+    file_name = "/Users/abdursiddiqui/Desktop/Security App"
+    sql_create_videos_table = """
+    CREATE TABLE IF NOT EXISTS videos ( 
+        id INTEGER PRIMARY KEY,
+        file_name VARCHAR(150),
+        date_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+    );"""
+
+    try:
+        conn = sqlite3.connect(db_file)
+        print(sqlite3.version)
+
+        # Create the users table
+        create_table(conn, sql_create_users_table)
+        print("Users table is created")
+
+        # Create the videos table
+        create_table(conn, sql_create_videos_table)
+        print("Videos table is created")
+
+    except Error as e:
+        print(e)
+    finally:
+        if conn:
+            conn.close()
+
+
+try:
+    conn = sqlite3.connect(db_file)
+    print(f"Connected to the database at: {db_file}")
+except Error as e:
+    print(e)
